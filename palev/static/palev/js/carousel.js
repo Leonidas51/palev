d685893ead8409e6e4fa94ef3dd57b4356cd74fb
Leonidas51 <kirpichik61@gmail.com>
2019-10-18 16:32:55 +0300

    carousel - out!

--- palev/static/palev/js/carousel.js
+++ palev/static/palev/js/carousel.js
@@ -1,33 +1,33 @@
 (function() {
   window.addEventListener('load', function() {
-    let items = document.getElementsByClassName('index-pic'),
+    let items = document.getElementsByClassName('carousel__container'),
         index = 0,
         timer = null;
 
-    document.querySelector('.index-image-1').classList.add('animate');
+    document.querySelector('.carousel__image_initial').classList.add('animate');
 
     setInterval(function() {
-      let image = items[index].querySelector('.index-image');
+      let image = items[index].querySelector('.carousel__image');
 
-      items[index].classList.remove('index-pic-current');
+      items[index].classList.remove('carousel__container_current');
 
       timer = setTimeout(stopAnimation(index), 500);
 
       if (items[index + 2]) {
-        items[index + 2].classList.add('index-pic-next');
+        items[index + 2].classList.add('carousel__container_next');
       } else {
-        items[0].classList.add('index-pic-next');
+        items[0].classList.add('carousel__container_next');
       }
 
       if (items[index + 1]) {
-        items[index + 1].classList.remove('index-pic-next');
-        items[index + 1].classList.add('index-pic-current');
-        items[index + 1].querySelector('.index-image').classList.add('animate');
+        items[index + 1].classList.remove('carousel__container_next');
+        items[index + 1].classList.add('carousel__container_current');
+        items[index + 1].querySelector('.carousel__image').classList.add('animate');
       } else {
-        items[0].classList.remove('index-pic-next');
-        items[0].classList.add('index-pic-current');
-        items[0].querySelector('.index-image').classList.add('animate');
-        items[1].classList.add('index-pic-next');
+        items[0].classList.remove('carousel__container_next');
+        items[0].classList.add('carousel__container_current');
+        items[0].querySelector('.carousel__image').classList.add('animate');
+        items[1].classList.add('carousel__container_next');
 
         index = -1;
       }
@@ -38,7 +38,7 @@
 
   function stopAnimation(index) {
     return function() {
-      document.getElementsByClassName('index-pic')[index].querySelector('.index-image').classList.remove('animate');
+      document.getElementsByClassName('carousel__container')[index].querySelector('.carousel__image').classList.remove('animate');
     }
   }
 
